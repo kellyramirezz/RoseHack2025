a900a617b77f10782f948cf602479214324a2357
Kelly Ramirez <157239272+kellyramirezz@users.noreply.github.com.>
2025-01-26 11:36:20 -0800

    updating js and added a score feature

--- script.js
+++ script.js
@@ -2,35 +2,52 @@
 function gameFunc(){
 
     document.getElementById("gameStart").style.display = 'none'; //makes the button disappear when clicked
-
     document.getElementById("btnContainer").style.display = "block"; //make trash bins appear 
+    document.getElementById("scoreContainer").style.display = "block";
 
 
-    //an array of 7 items images
-    let myOps = new Array(
-        'IMG1.PNG',
-        'IMG2.PNG',
-        'IMG3.PNG',
-        'IMG4.PNG',
-        'IMG5.PNG',
-        'IMG6.PNG',
-        'IMG7.PNG',
-    );
+    // an array of 7 items images
+    // let myOps = new Array(
+    //     'IMG1.PNG',
+    //     'IMG2.PNG',
+    //     'IMG3.PNG',
+    //     'IMG4.PNG',
+    //     'IMG5.PNG',
+    //     'IMG6.PNG',
+    //     'IMG7.PNG',
+    // );
 
+    // randomNum = Math.floor(Math.random() * myOps.length);
+    // randImg = myOps[randomNum]; //chooses a random img from folder
+    // console.log(randImg);
+
+    // //display imgage
+    // document.getElementById('imgPlace').src = `./img/guessImg/${randImg}`
+    
+    // -----------------
+    // tryna figure our how to make statements of correct answers
+}
+
+//scores
+var score = 0; // start w score 
+
+    const myOps = ["IMG1.PNG", "IMG2.PNG", "IMG3.PNG"];
     randomNum = Math.floor(Math.random() * myOps.length);
     randImg = myOps[randomNum]; //chooses a random img from folder
+    console.log(randImg);
 
-    //display imgage
     document.getElementById('imgPlace').src = `./img/guessImg/${randImg}`
 
-    // -----------------
-    //tryna figure our how to make statements of correct answers
-    checkAnswer();
+function checkAnswer(userChoice){
+
+    // let guesses = ['1,2,3,4,5,6,7'];
+
+    switch(userChoice + guesses){
+        case "glassIMG6.PNG":
+            alert('trueeee');
+            break;
+    }
 }
 
-function checkAnswer(){
-    var score = 0; // start w score 0
-
-}
 
 //i think needing to save which randIMG to compare w trash can needs to be made
