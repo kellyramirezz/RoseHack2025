7b88f27f55a1a8963225dc2a972c0a9a50105c94
Kelly Ramirez <157239272+kellyramirezz@users.noreply.github.com.>
2025-01-28 17:25:16 -0800

    Added score feature and wrong feature and added more css to the buttons

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function gameFunc(){
 
     document.getElementById("gameStart").style.display = 'none'; //makes the button disappear when clicked
     document.getElementById("btnContainer").style.display = "block"; //make trash bins / items appear 
+    document.getElementById("scoreContainer").style.display = "block";
 
     //display imgage
     document.getElementById('imgPlace').src = `./img/guessImg/${window.GLOBALITEMnum}`
@@ -37,34 +38,58 @@ function checkA(userC){
     switch(c + window.GLOBALITEMnum){
         case 'gIMG6.PNG':
             console.log(c + window.GLOBALITEMnum); //testing purposes
-            alert('TRUE glass');
+            // alert('TRUE glass');
+            win();
             break;
         case 'hIMG7.PNG':
             console.log(c + window.GLOBALITEMnum);
-            alert('TRUE haz');
+            // alert('TRUE haz');
+            win();
             break;
         case 'paIMG4.PNG':
             console.log(c + window.GLOBALITEMnum);
-            alert('TRUE paper');
+            // alert('TRUE paper');
+            win();
             break;
         case 'mIMG2.PNG':
             console.log(c + window.GLOBALITEMnum);
-            alert('TRUE metal');
+            // alert('TRUE metal');
+            win();
             break;
         case 'oIMG5.PNG':
             console.log(c + window.GLOBALITEMnum);
-            alert('TRUE organic');
+            // alert('TRUE organic');
+            win();
             break;
         case 'plaIMG1.PNG':
             console.log(c + window.GLOBALITEMnum);
-            alert('TRUE plastic');
+            // alert('TRUE plastic');
+            win();
             break;
         case 'wIMG3.PNG':
             console.log(c + window.GLOBALITEMnum);
-            alert('TRUE waste');
+            // alert('TRUE waste');
+            win();
             break;
+        default:
+            wrong();
     }
 
+
     //reload window each time after win, will fix later when score is added
-    window.location.reload();
+    window.GLOBALITEMnum = NUMBERCOMP();
+    document.getElementById('imgPlace').src = `./img/guessImg/${window.GLOBALITEMnum}`
+}
+
+let userScore = 0;
+// const results_div = document.querySelector(".results");
+
+function win(){
+    ++userScore;
+    document.getElementById("userScoreBox").innerHTML = userScore;
+    console.log(userScore);
+}
+
+function wrong(){
+    document.getElementById("wrongA").innerHTML = "Wrong Answer, Try Again";
 }
